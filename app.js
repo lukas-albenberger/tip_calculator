bd8fe6cc843325d7548fe665a31cd005734eb814
Baradil <54547833+Baradil@users.noreply.github.com>
2020-07-01 16:17:06 +0200

    Started with the skeletton
    Skeletton for HTML; CSS & JavaScript

---
+++ app.js
@@ -0,0 +1,34 @@
+function calculateTip() {
+    var billAmount = document.getElementById("billAmt").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numOfPeople = document.getElementById("peopleAmount").value;
+
+    if (billAmount === "" || serviceQuality == 0) {
+        alert("please enter values");
+        return;
+    }
+
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+    
+    document.getElementById("each").style.display = "none"; }
+    else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    const total = (billAmount * serviceQuality) / numOfPeople;
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+
+    document.getElementById("totalTip").style.display = "none";
+    document.getElementById("each").style.display = "none";
+
+    document.getElementById("calculate").onclick = function()
+    {
+        calculateTip();
+    
+};
+}
